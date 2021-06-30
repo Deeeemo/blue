2ec7c3de5ec93d5fbdbeed2342c4d8ce09152df9
Michael Nematollahi <m.nematollahi99@gmail.com>
2021-06-29 20:25:31 -0700

    Move Z to a function.

--- Strings/Z.cpp
+++ Strings/Z.cpp
@@ -2,33 +2,33 @@
 
 using namespace std;
 
-const int MAXN = 1e6 + 10;
-
-string s;
-int n, z[MAXN];
+vector<int> get_z(string s) {
+    int n = s.size();
+    vector<int> z(n);
+    int l = 0, r = 0;
+    for (int i = 1; i < n; i++)
+        if (r <= i){
+            l = r = i;
+            while (r < n && s[r] == s[r-l]) r++;
+            z[i] = r-i;
+        }
+        else{
+            if (z[i-l] < (r - i))
+                z[i] = z[i-l];
+            else{
+                l = i;
+                while (r < n && s[r] == s[r-l]) r++;
+                z[i] = r-i;
+            }
+        }
+    return z;
+}
 
 int main(){
-	cin >> s;
-	n = s.size();
-	int l = 0, r = 0;
-	for (int i = 1; i < n; i++)
-		if (r <= i){
-			l = r = i;
-			while (r < n && s[r] == s[r-l]) r++;
-			z[i] = r-i;
-		}
-		else{
-			if (z[i-l] < (r - i))
-				z[i] = z[i-l];
-			else{
-				l = i;
-				while (r < n && s[r] == s[r-l]) r++;
-				z[i] = r-i;
-			}
-		}
-
-	for (int i = 0; i < n; i++)
-		cout << z[i] << " ";
-	cout << "\n";
+	string s; cin >> s;
+    auto z = get_z(s);
+    for (auto x: z)
+        cout << x << " ";
+    cout << "\n";
 	return 0;
 }
